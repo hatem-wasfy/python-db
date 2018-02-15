6a14d8df058a4ca2c0839c5c1b72403c05ef0198
hatem-wasfy <34537566+hatem-wasfy@users.noreply.github.com>
2018-02-16 00:40:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ from flask_heroku import Heroku
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['postgres://yrkgfdwphwaktc:c5b75242c71781f8dbdb1df56f91ae89cc291dcbeb54fd88d3b543ebf5b4a207@ec2-107-22-175-33.compute-1.amazonaws.com:5432/def7m51jhsgoca
-'] = os.environ['ec2-107-22-175-33.compute-1.amazonaws.com']
+app.config['postgres://yrkgfdwphwaktc:c5b75242c71781f8dbdb1df56f91ae89cc291dcbeb54fd88d3b543ebf5b4a207@ec2-107-22-175-33.compute-1.amazonaws.com:5432/def7m51jhsgoca'] = os.environ['ec2-107-22-175-33.compute-1.amazonaws.com']
 db = SQLAlchemy(app)
            
            
