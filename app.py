f3ba126ae67d514f8a0e565ded64e9c17b64236d
hatem-wasfy <34537566+hatem-wasfy@users.noreply.github.com>
2018-02-15 22:56:28 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import urllib
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = makeWebhookResult(req)
+
+    res = json.dumps(res, indent=4)
+    print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+def makeWebhookResult(req):
+
+    result = req.get("result")
+    parameters = result.get("parameters")
+    city = parameters.get("city")
+
+    resolvedQuery = parameters.get("resolvedQuery")
+    attraction = parameters.get("attraction")    
+
+    
+    
+
+    ###speech = "There are nice" + attraction #+ "places in " + city + "to have" + resolvedQuery + " and I will tell you about them. " #+ str(cost[zone])
+    ##working### speech = "There are nice places in " + city + " and I will tell you about them. "
+    #speech = "There are nice " + attraction + " to have " + resolvedQuery + "places in " + city + " and I will tell you about them. "
+    #speech = "There are nice places in " + city + attraction + resolvedQuery + " and I will tell you about them. "
+    speech = "Hmm, I'll tell you about the best places in " + city + " to have " + attraction
+    
+    print("Response:")
+    print(speech)
+    return {
+        "speech": speech,
+        "displayText": speech,
+        #"data": {},
+        #"contextOut": [],
+        ##"source": "travelsourse"
+    }
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print ("Starting app on port %d" %(port))
+
+    app.run(debug=True, port=port, host='0.0.0.0')
