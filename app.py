aab7c944abf8b80e6c9133f8c886ff555604f99e
hatem-wasfy <34537566+hatem-wasfy@users.noreply.github.com>
2018-02-16 18:17:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -30,10 +30,10 @@ conn = psycopg2.connect(
 city = "Fukuoka"
 attraction = "food"
 places = "a1 a2"
-#lastchangedate = "DEFAULT"
+lastchangedate = "DEFAULT"
     
 query =  "INSERT INTO japandb (city, attraction, places, lastchangedate) VALUES (%s, %s, %s, %t);"
-data = (city, attraction, places, DEFAULT)
+data = (city, attraction, places, lastchangedate)
 
 curs = conn.cursor()
 curs.execute(query, data)
