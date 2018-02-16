8c8294b54dcce05835112481790326f4e6186985
hatem-wasfy <34537566+hatem-wasfy@users.noreply.github.com>
2018-02-16 17:10:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,16 +12,59 @@ app = Flask(__name__)
 #parse.uses_netloc.append("postgres")
 #url = parse.urlparse(os.environ["postgres://swkjnmiksbabsb:c9a348935037d022f6450c7deb6df6ad9d846d736bf04c44b52338092f046554@ec2-174-129-221-240.compute-1.amazonaws.com:5432/d50jsgearpigne"])
 
-conn = psycopg2.connect(
-    database="d50jsgearpigne",
-    user="swkjnmiksbabsb",
-    password="c9a348935037d022f6450c7deb6df6ad9d846d736bf04c44b52338092f046554",
-    host="ec2-174-129-221-240.compute-1.amazonaws.com",
-    port=5432
-)
+#############******************************************
+
+###conn = psycopg2.connect(
+    ###database="d50jsgearpigne",
+    ###user="swkjnmiksbabsb",
+    ###password="c9a348935037d022f6450c7deb6df6ad9d846d736bf04c44b52338092f046554",
+    ###host="ec2-174-129-221-240.compute-1.amazonaws.com",
+    ###port=5432
+###)
+
+#############******************************************
 
 
+##cur = conn.cursor()
+##cur.execute(sql, (value1,value2))
 
+##cur.close()
+##conn.close()
+
+get_vendor()
+
+
+def get_vendors():
+    """ query data from the vendors table """
+    conn = None
+    try:
+        conn = psycopg2.connect(
+            database="d50jsgearpigne",
+            user="swkjnmiksbabsb",
+            password="c9a348935037d022f6450c7deb6df6ad9d846d736bf04c44b52338092f046554",
+            host="ec2-174-129-221-240.compute-1.amazonaws.com",
+            port=5432
+        )
+        
+        cur = conn.cursor()
+        #cur.execute("SELECT vendor_id, vendor_name FROM vendors ORDER BY vendor_name")
+        cur.execute("SELECT * FROM japandb")
+        
+        print("The number of parts: ", cur.rowcount)
+        row = cur.fetchone()
+ 
+        while row is not None:
+            print(row)
+            row = cur.fetchone()
+ 
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            
+            
 
 # Config
 
