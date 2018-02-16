e92fca236ef86dbb86c3fcbe49b081031b5e839d
hatem-wasfy <34537566+hatem-wasfy@users.noreply.github.com>
2018-02-16 16:00:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -43,6 +43,13 @@ conn = psycopg2.connect(
         ##self.url = url
         
         
+##if __name__ == '__main__':
+    ##app.run(debug=True)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.getenv('PORT', 5000))
+
+    print ("Starting app on port %d" %(port))
+
+    app.run(debug=True, port=port, host='0.0.0.0')
     
